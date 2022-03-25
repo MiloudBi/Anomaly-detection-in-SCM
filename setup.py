fff67b94d90a5f3c5675f0f65dfe037d75c2db94
Miloud SEFIANI <sefiani.miloud96@gmail.com>
2022-03-25 03:07:26 +0100

    setuptools about my project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Anomaly-detection-in-SCM',
+    author='Miloud SEFIANI',
+    description='An evaluation of multivariate time-series anomaly detection algorithms',
+    long_description=open('README.md').read(),
+    version='0.0',
+    packages=find_packages(),
+    scripts=[],
+)
